3e0f5204416683815de9267f9fb88bfc343fb590
Graeme Ross <graeme@oneutilitybill.co>
2016-07-25 17:40:28 +0100

    Removed unused model

--- Plugins/models/virginAddress.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-class VirginAddress {
-    constructor(address = {}) {
-        this.addressLine1 = address.addressLine1 || '';
-        this.city = address.city || '';
-        this.postcode = address.postcode || '';
-    }
-}
